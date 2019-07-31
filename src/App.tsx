c518b8d5d4cc9e92d430ea806b70388f86bc3ac4
Amal <amalchandra97@hotmail.com>
2019-07-31 21:35:04 +1200

    Facebook auth working

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,10 @@ import './App.css';
 // import { Button } from '@material-ui/core';
 import Header from './Components/Header';
 import JobList from './Components/JobList';
+import * as firebase from 'firebase/app'
+import 'firebase/auth';
 import Card from 'react-bootstrap/Card';
+import StyledFirebaseAuth from "react-firebaseui/StyledFirebaseAuth";
 
 
 // import logo from './logo.svg';
@@ -12,21 +15,46 @@ import Card from 'react-bootstrap/Card';
 interface IState {
   locationSearch: string
   inputSearch: string
+  isSignedIn:boolean
   jobList: object
   updateJobList: any
 }
 
+  firebase.initializeApp({
+    apiKey:"AIzaSyAM5UQGYIRCd845-OdWSmwtS0DQdhyKmbk",
+    authDomain:"findmeajob-b108e.firebaseapp.com"
+  })
+
+
 class App extends React.Component<{}, IState>{
+    private uiConfig = {
+    Callbacks: {
+      signInSuccess: () => false
+    },
+    signInFlow: "popup",
+    signInOptions: [
+      firebase.auth.FacebookAuthProvider.PROVIDER_ID,
+    ]
+  }
   public constructor(props: any) {
     super(props);
     this.state = {
       inputSearch: "",
+      isSignedIn: false,
       jobList: [],
       locationSearch: "",
       updateJobList: null
     }
   }
 
+  
+  public componentDidMount = () =>{
+  firebase.auth().onAuthStateChanged(user => {
+    this.setState({isSignedIn: !!user })
+    console.log("user", user)
+    })
+  }
+  
   public updateJobList =() =>{
         console.log("hey");
   }
@@ -77,11 +105,22 @@ class App extends React.Component<{}, IState>{
 
   public render() {
     return (
-       <div>
-         <Header findJobs={this.findJobs} />
-         <JobList mount={this.listMounted} />
-                         {this.state.jobList}
-
+       <div className="App">
+         {this.state.isSignedIn ? (
+           <span>
+             <div> Signed In !</div>
+             <button onClick={()=>firebase.auth().signOut()}> Sign Out!</button>
+             <h1>Welcome {firebase.auth().currentUser!.displayName} </h1>
+             <Header findJobs={this.findJobs} />
+             <JobList mount={this.listMounted} />
+             {this.state.jobList}
+          </span>
+         ):(
+           <StyledFirebaseAuth
+              uiConfig={this.uiConfig}
+              firebaseAuth={firebase.auth()}
+            />
+         )}
        </div>
 
       
